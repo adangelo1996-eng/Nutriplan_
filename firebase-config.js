569a2136ba05443441bc53a0a7cd10dcd78054aa
adangelo1996-eng <a.dangelo1996@gmail.com>
2026-02-19 20:53:47 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBitbmjthQUvKyrHmADVLkvwENoVrXYxNY",
-  authDomain: "nutriplan-100f9.firebaseapp.com",
-  databaseURL: "https://nutriplan-100f9-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nutriplan-100f9",
-  storageBucket: "nutriplan-100f9.firebasestorage.app",
-  messagingSenderId: "371119799995",
-  appId: "1:371119799995:web:309e59696041f3bbc9739e",
-  measurementId: "G-VELZ8ZZYQ3"
+var firebaseConfig = {
+    apiKey: "AIzaSyBitbmjthQUvKyrHmADVLkvwENoVrXYxNY",
+    authDomain: "nutriplan-100f9.firebaseapp.com",
+    databaseURL: "https://nutriplan-100f9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nutriplan-100f9",
+    storageBucket: "nutriplan-100f9.firebasestorage.app",
+    messagingSenderId: "371119799995",
+    appId: "1:371119799995:web:309e59696041f3bbc9739e",
+    measurementId: "G-VELZ8ZZYQ3"
 };
