0debf5a86b6e8d1369f983bbc927d7a30fd42574
adangelo1996-eng <a.dangelo1996@gmail.com>
2026-02-19 20:23:49 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyBitbmjthQUvKyrHmADVLkvwENoVrXYxNY",
   authDomain: "nutriplan-100f9.firebaseapp.com",
   databaseURL: "https://nutriplan-100f9-default-rtdb.europe-west1.firebasedatabase.app",
@@ -17,6 +12,3 @@ const firebaseConfig = {
   measurementId: "G-VELZ8ZZYQ3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
